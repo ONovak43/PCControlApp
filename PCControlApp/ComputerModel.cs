328e4e384202894c8334b41de52dd0917a71c677
Ondřej Novák <ondrej.kt@seznam.cz>
2019-12-10 19:43:57 +0100

    Initial commit

---
+++ PCControlApp/ComputerModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PCControlApp
+{
+    public class ComputerModel
+    {
+        /// <summary>
+        /// Represents the domain (host) for this particular computer.
+        /// </summary>
+        public string Domain { get; }
+
+        /// <summary>
+        /// Represents the MAC address for this particular computer.
+        /// </summary>
+        public string MacAddress { get; }
+
+
+        public ComputerModel(string domain, string macAddress)
+        {
+            if (!IsMacAddress(macAddress))
+                throw new ArgumentException(message: "Invalid argument", paramName: nameof(macAddress));
+
+            Domain = domain;
+            MacAddress = macAddress;
+        }
+
+        public async void Start() => await Network.WakeOnLan(MacAddress);
+
+
+        public async Task<bool> IsRunning() => await Network.Ping(Domain);
+
+
+        private bool IsMacAddress(string address) => Regex.IsMatch(address, "^((([a-fA-F0-9][a-fA-F0-9]+[-]){5}|" +
+                "([a-fA-F0-9][a-fA-F0-9]+[:]){5})([a-fA-F0-9][a-fA-F0-9])$)|" +
+                "(^([a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]+[.]){2}" +
+                "([a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]))$");
+    }
+}
---
+++ PCControlLibrary/DataAccess/IDataConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlLibrary.DataAccess
+{
+    public interface IDataConnection
+    {
+        SettingModel GetComputers(SettingModel model);
+    }
+}
